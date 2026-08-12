b5acd5d120ca7b143db19ee6b6baaf6bfc139ce4
blackystrngr <kisalk2007@outlook.com>
2026-08-12 19:04:58 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -8,4 +8,4 @@ cd ..
 rm -rf ffmpeg-master-latest-linux64-gpl*
 
 # install pip packages
-pip install -r requirements.txt --break-system-packages --ignore-installed
+pip3 install -r requirements.txt --break-system-packages --ignore-installed
