e36c1d63399819896a8fc6d61c463f39c657bdc1
blackystrngr <kisalk2007@outlook.com>
2026-08-12 16:18:04 +0530

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# install ffmpeg
+wget https://github.com/BtbN/FFmpeg-Builds/releases/download/latest/ffmpeg-master-latest-linux64-gpl.tar.xz
+tar -xf ffmpeg-master-latest-linux64-gpl.tar.xz
+cd ffmpeg-master-latest-linux64-gpl/bin
+sudo mv ffmpeg ffplay ffprobe /usr/local/bin/
+cd ..
+rm -rf ffmpeg-master-latest-linux64-gpl*
+
+# install pip packages
+pip install -r requirements.txt --break-system-packages --ingore-installed
